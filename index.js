2cb207986aff7f70fd90a104312a6b88582bbab3
Kashif Ahmed <kashifahmed2604@gmail.com>
2026-01-14 16:18:22 +0530

    Phase 1: Setup Express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("MERN Todo Backend Running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
