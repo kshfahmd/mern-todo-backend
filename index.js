f2f9f46e7452403153eee5cb796c8647fffcf98c
Kashif Ahmed <kashifahmed2604@gmail.com>
2026-01-14 18:21:06 +0530

    Phase2 : Connect MongoDB with dotenv + mongoose

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();
+
+const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("MERN Todo Backend Running");
+  res.send("MERN Todo Backend Running ✅");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+startServer();
